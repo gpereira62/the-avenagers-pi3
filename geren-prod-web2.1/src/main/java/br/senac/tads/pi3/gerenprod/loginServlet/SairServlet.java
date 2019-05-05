49a8ec7d61a38fa1017c354b3030bc4e40a89fa0
Bruna Duarte <brubsduarte3093@gmail.com>
2019-05-05 00:52:09 -0300

    Criado o logout do usuário.

---
+++ geren-prod-web2.1/src/main/java/br/senac/tads/pi3/gerenprod/loginServlet/SairServlet.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.senac.tads.pi3.gerenprod.loginServlet;
+
+import br.senac.tads.pi3.gerenprod.model.Usuario;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Bruna
+ */
+@WebServlet(name = "SairServlet", urlPatterns = {"/sair"})
+public class SairServlet extends HttpServlet {
+
+  @Override
+  protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    Usuario usuario = new Usuario();
+    usuario.setSession(request);
+    response.sendRedirect(request.getContextPath() + "/");
+  }
+}
