0c3b23a0700fb5345c0a2ee44c65c24cbdd69ea3
Bruna Duarte <brubsduarte3093@gmail.com>
2019-05-05 23:52:22 -0300

    Corrigido a identação do dao de aluguel

--- geren-prod-web2.1/src/main/java/br/senac/tads/pi3/gerenprod/dao/AluguelDAO.java
+++ geren-prod-web2.1/src/main/java/br/senac/tads/pi3/gerenprod/dao/AluguelDAO.java
@@ -15,71 +15,69 @@ import java.util.ArrayList;
  *
  * @author Gustavo
  */
-public class AluguelDAO implements CrudInterface<Aluguel>{
+public class AluguelDAO implements CrudInterface<Aluguel> {
 
+  @Override
+  public Aluguel mostrar(int ID) {
+    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+  }
 
+  @Override
+  public boolean editar(Aluguel objeto) {
+    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+  }
 
-    @Override
-    public Aluguel mostrar(int ID) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+  @Override
+  public boolean salvar(Aluguel a) {
+    DB db = new DB(false);
+
+    try {
+
+      String pattern = "yyyy-MM-dd";
+      DateFormat df = new SimpleDateFormat(pattern);
+
+      String sql
+        = "INSERT INTO aluguel "
+        + "(DataInicial, idCliente, idFilial, idProduto)"
+        + "VALUES ("
+        + "'" + df.format(a.getDataInicial()) + "', "
+        + a.getIdCliente() + ", "
+        + a.getIdFilial() + ", "
+        + a.getIdProduto() + ");";
+
+      if (!db.executarAlteracao(sql)) {
+        throw new Exception("N�o foi possivel cadastrar o aluguel.");
+      }
+
+      sql
+        = "UPDATE produto SET "
+        + "Alugado = 1 "
+        + "Where idProduto = " + a.getIdProduto() + ";";
+
+      if (!db.executarAlteracao(sql)) {
+        throw new Exception("N�o foi possivel cadastrar o aluguel.");
+      }
+
+      db.commit();
+      db.close();
+      return true;
+
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
+      db.rollback();
+      db.close();
+      return false;
     }
+  }
 
-    @Override
-    public boolean editar(Aluguel objeto) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+  @Override
+  public boolean desativar(int ID) {
+    return false;
+  }
 
-    @Override
-    public boolean salvar(Aluguel a) {
-        DB db = new DB(false);
+  @Override
+  public ArrayList<Aluguel> listar(int idFilial) {
+    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+  }
 
-        try {
-
-            String pattern = "yyyy-MM-dd";
-            DateFormat df = new SimpleDateFormat(pattern);
-
-            String sql
-                    = "INSERT INTO aluguel "
-                    + "(DataInicial, idCliente, idFilial, idProduto)"
-                    + "VALUES ("
-                    + "'" + df.format(a.getDataInicial())+ "', "
-                    + a.getIdCliente()+ ", "
-                    + a.getIdFilial()+ ", "
-                    + a.getIdProduto()+ ");";
-
-            if (!db.executarAlteracao(sql)) {
-                throw new Exception("N�o foi possivel cadastrar o aluguel.");
-            }
-            
-            sql
-                    = "UPDATE produto SET "
-                    + "Alugado = 1 "
-                    + "Where idProduto = " + a.getIdProduto() + ";";
-
-            if (!db.executarAlteracao(sql)) {
-                throw new Exception("N�o foi possivel cadastrar o aluguel.");
-            }
-            
-            db.commit();
-            db.close();
-            return true;
-
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            db.rollback();
-            db.close();
-            return false;
-        }
-    }
-
-    @Override
-    public boolean desativar(int ID) {
-        return false;
-    }
-
-    @Override
-    public ArrayList<Aluguel> listar(int idFilial) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-    
 }
