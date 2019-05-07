bde94e383b31997f5092c78ccc07fe95f559e33a
Alexandre Macbook <alexandreviniciusfs@gmail.com>
2019-05-06 22:24:36 -0300

    relatorio 1.0

---
+++ geren-prod-web2.1/src/main/java/br/senac/tads/pi3/gerenprod/dao/RelatorioDAO.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.senac.tads.pi3.gerenprod.dao;
+
+import br.senac.tads.pi3.gerenprod.db.DB;
+import br.senac.tads.pi3.gerenprod.model.Relatorio;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author mac-ale
+ */
+public class RelatorioDAO implements CrudInterface<Relatorio> {
+
+    @Override
+    public ArrayList<Relatorio> listar(int idFilial) {
+        DB db = new DB(true);
+        try {
+            String sql = "SELECT\n"
+                    + "idAluguel,\n"
+                    + "Nome,\n"
+                    + "NomeProduto,\n"
+                    + "PrecoDiaria,\n"
+                    + "ValorTotal\n"
+                    + "FROM Aluguel\n"
+                    + "INNER JOIN Cliente ON Cliente.idCliente = Aluguel.idCliente\n"
+                    + "INNER JOIN Produto ON Produto.idProduto = Aluguel.idProduto";
+
+            ResultSet rs = db.executarConsulta(sql);
+            Relatorio relat = new Relatorio();
+            ArrayList<Relatorio> relatorios = new ArrayList<>();
+            while (rs.next()) {
+                relat.setIdAluguel(rs.getInt("idAluguel"));
+                relat.setNomeCliente(rs.getString("Nome"));
+                relat.setPrecoDiaria(rs.getDouble("PrecoDiaria"));
+                relat.setValorTotal(rs.getDouble("ValorTotal"));
+                relatorios.add(relat);
+            }
+
+            db.close();
+            return relatorios;
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+            db.close();
+            return null;
+        }
+    }
+
+    @Override
+    public Relatorio mostrar(int ID) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean editar(Relatorio objeto) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean salvar(Relatorio objeto) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean desativar(int ID) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
---
+++ geren-prod-web2.1/src/main/java/br/senac/tads/pi3/gerenprod/model/Relatorio.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.senac.tads.pi3.gerenprod.model;
+
+/**
+ *
+ * @author mac-ale
+ */
+public class Relatorio {
+    int idAluguel;
+    String nomeCliente, NomeProduto;
+    double PrecoDiaria;
+    double ValorTotal;
+    
+    public int getIdAluguel() {
+        return idAluguel;
+    }
+
+    public void setIdAluguel(int idAluguel) {
+        this.idAluguel = idAluguel;
+    }
+
+    public String getNomeCliente() {
+        return nomeCliente;
+    }
+
+    public void setNomeCliente(String nomeCliente) {
+        this.nomeCliente = nomeCliente;
+    }
+
+    public String getNomeProduto() {
+        return NomeProduto;
+    }
+
+    public void setNomeProduto(String NomeProduto) {
+        this.NomeProduto = NomeProduto;
+    }
+
+    public double getPrecoDiaria() {
+        return PrecoDiaria;
+    }
+
+    public void setPrecoDiaria(double PrecoDiaria) {
+        this.PrecoDiaria = PrecoDiaria;
+    }
+
+    public double getValorTotal() {
+        return ValorTotal;
+    }
+
+    public void setValorTotal(double ValorTotal) {
+        this.ValorTotal = ValorTotal;
+    }
+
+  
+ }
